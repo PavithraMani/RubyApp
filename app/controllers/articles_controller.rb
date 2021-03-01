02f224c0afd05bad553391be788dbc6ca863a0dd
pavimanipnm <79173319+pavimanipnm@users.noreply.github.com>
2021-03-01 15:37:09 -0800

    Make app prod ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   # GET /articles or /articles.json
   def index
     @articles = Article.all
@@ -6,6 +8,47 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
-    @article = Article.find(params[:id]);
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article #extracts the ID of the @article and redirects to article/:id
+    else
+      render 'new' #new.html.erb
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article #extracts the ID of the @article and redirects to article/:id
+    else
+      render 'edit' #update.html.erb
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
