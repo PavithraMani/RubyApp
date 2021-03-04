9ff13ab4b2b2db9292882ebd8a8e5b27679b2687
pavimanipnm <79173319+pavimanipnm@users.noreply.github.com>
2021-03-04 14:35:53 -0800

    add users associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_220049) do
+ActiveRecord::Schema.define(version: 2021_03_04_222910) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
