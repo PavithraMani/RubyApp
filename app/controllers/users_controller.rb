1b671d79f4545d9158a65b75816aa143a2d8e503
pavimanipnm <79173319+pavimanipnm@users.noreply.github.com>
2021-03-05 10:26:30 -0800

    user links in artcles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,18 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
 
+  def index
+    @users = User.all
+  end
+
   def edit
   end
 
   def create
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome #{@user.username}! You have successfully signed up."
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'new' #new.html.erb
     end
