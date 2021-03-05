0d6068effb7b9527aad69735f5a3db602aa51dc7
pavimanipnm <79173319+pavimanipnm@users.noreply.github.com>
2021-03-05 08:57:41 -0800

    edit-user-functionality contd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,40 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user=User.new
   end
 
   def show
-  end
-
-  def edit
     @user = User.find(params[:id])
   end
 
+  def edit
+  end
+
   def create
-    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome #{@user.username}! You have successfully signed up."
-      redirect_to articles_path #extracts the ID of the @article and redirects to article/:id
+      redirect_to articles_path
     else
       render 'new' #new.html.erb
     end
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account was updated successfully."
-      redirect_to articles_path #extracts the ID of the @article and redirects to article/:id
+      redirect_to articles_path
     else
       render 'edit' #update.html.erb
     end
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
